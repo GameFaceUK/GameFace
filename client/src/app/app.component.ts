6392492588b0c4faa9e43de0d7e6d17e6e85157e
MattJSmith <matthewjs@outlook.com>
2017-02-25 12:22:50 +0000

    (ui) : given app component a html to follow normal structure

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'game-face-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl: 'app/app.component.html',
 })
 export class AppComponent {
   name = 'Game Face';
